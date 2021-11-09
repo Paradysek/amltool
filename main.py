cefbca404eca1b12257ea0ffb3c2f7ff55a9ba63
Mateusz <skipper126@interia.pl>
2021-11-09 20:54:28 +0100

    added example of getting data from web api

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+import webapi
+
 print("Hello World!")
 print('hello from mateurz')
 print("O Dzień dobry")
+
+
+# example of getting exchange rate from NBP
+
+res = webapi.get_exchange_rate('A', 'CHF')
+json_response = res.json()
+print(json_response['rates'][0]['mid'])
+
+
